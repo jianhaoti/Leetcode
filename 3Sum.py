e13f7dd9b30c880b726ae678bbe3ab99825c958b
Paul Tee <jianhaoti@gmail.com>
2023-10-22 14:19:32 -0400

    fixed 3sum

--- 3Sum.py
+++ 3Sum.py
@@ -1,39 +1,35 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        #hard to beat O(n^2) since three equations
-        #and 1 constraint => 2 degrees of freedom.
+        #O(n^2) algorithm
+        #first sort.
+        #then, define 3 pointers, one "wall" and a left/right pointer wrt the wall. these two pointer go to the wall
+        #the stopping condition (which increments the wall by 1) is when one of the left/right
+        #pointers hits the wall. now run sorted 2sum on the left/right pointers.
+        #if you hit a solution, you in/dec-crement both pointers by 1 simulatenously.
+        #this works since you have 3-1=2 degrees of freedom! if the problem was 4sum, we would be
+        #forced to make a 2-nary tree here, where left index increments and right index decrements
 
-        #howver, we can push this to nlogn
-        #the idea is to sort: O(nlogn)
-        #then use 2 pointer method on sorted 2sum + varying pointer with binary search
-
-        #### #### ####
         nums.sort()
+
+        #initialize as a hashmap to count unique solutions
+        solutions = {}
         n = len(nums)
-        solutions = []
-        pointerLeft = 0
-        pointerRight = n-1
+        for wall in range(1,n-1):
+            left = 0
+            right = n-1
+            while left != wall and right != wall:
+                sum = nums[left] + nums[right] + nums[wall]
+                if sum == 0:
+                    found = (nums[left], nums[wall], nums[right])
+                    if found not in solutions:
+                        solutions.update({found:1})
+                    left+=1
+                    right-=1
+                elif sum > 0:
+                    right-=1
+                elif sum <0:
+                    left+=1
 
-        while pointerLeft < pointerRight:
-            answer = self.bSearch(nums, pointerLeft, pointerRight, 0 - nums[pointerLeft] - nums[pointerRight])
-            pointerLeft+=1
-            pointerRight-=1
-            if answer is not None:
-                solutions.append([nums[pointerLeft], nums[pointerRight], nums[answer]])
+        return list(solutions)
 
-        return solutions
 
-    ##left and right are pointers, seek is an desired element in arr
-    def bSearch(self, arr: List[int], left: int, right:int, seek:int) -> int:
-        midpoint = left+right//2
-
-        if right-left>=2:
-            if seek == arr[midpoint]:
-                return seek
-            elif seek > arr[midpoint]:
-                self.bSearch(arr, midpoint, right, seek)
-            elif seek < arr[midpoint]:
-                self.bSearch(arr, left, midpoint, seek)
-
-        #if no solution
-        return None

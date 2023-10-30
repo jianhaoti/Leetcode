8b2f80b5728b4222b4c2eef23a4e6dc168cbbcf7
Paul Tee <jianhaoti@gmail.com>
2023-10-30 16:02:02 -0400

    works but piss slow lol

--- SearchA2DMatrixV2.py
+++ SearchA2DMatrixV2.py
@@ -1,23 +1,39 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m = len(matrix) - 1
-        n = len(matrix[0]) - 1
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        maxValue = 0
+        n = len(piles)
 
-        return self.bSearchMatrix(matrix, target, (0,0), (m,n))
+        for element in piles: #O(n)
+            maxValue = max(element, maxValue)
 
-    def bSearchMatrix(self, matrix: List[List[int]], target:int, left:tuple, right:tuple) -> bool:
-        while left[0] <= right[0] and left[1] <= right[1]:
-            middle = ((left[0]+left[0])//2, (right[1]+right[1])//2)
-            if matrix[middle[0]][middle[1]] > target:
-                if middle[1] != 0: #not frist element in row
-                    right = (middle[0], middle[1] - 1)
-                else:
-                    right = (middle[0]-1, len(matrix[0]))
-            elif matrix[middle[0]][middle[1]] < target:
-                if middle[1] != len(matrix[0]) - 1: #not last element in row
-                    left = (middle[0], middle[1] + 1)
-                else:
-                    left = (middle[0]+1, 0)
+        left, right = 0, maxValue
+        print("Search space from", left, "to", right)
+
+        while left < right:
+            mid = (left + right)//2
+            print("Mid:", mid)
+            if self.fastEnough(piles, h, mid):
+                right = mid - 1
+                print("Search space from", left, "to -", right, "-")
             else:
-                return True
+                left = mid + 1
+                print("Search space from -", left, "- to", right)
+        if self.fastEnough(piles, h, right):
+            return right
+        return right+1
+
+    def fastEnough(self, piles: List[int], h:int, k:int) -> bool:
+        time = 0
+        if k != 0:
+            for element in piles:
+                if element%k != 0:
+                    element = element - element%k + k
+                time += element/k
+
+            print("Total time is:", time)
+            return time <= h
         return False
+
+
+
+

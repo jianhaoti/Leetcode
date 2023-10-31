25cf64a94a5c9e03f5f81a487cc90f9fdcb5cb94
Paul Tee <jianhaoti@gmail.com>
2023-10-31 02:16:45 -0400

    housekeeping

---
+++ containsDuplicate/src/Solution.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.Map;
+class Solution{
+            public boolean containsDuplicate(int[] nums) {
+                Map<Integer, Integer> hashTable = new HashMap<>();
+                for
+            }
+}

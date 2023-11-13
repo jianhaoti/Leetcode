8fc94f97c5572c81668216d7b3da8b477d358e50
Paul Tee <jianhaoti@gmail.com>
2023-11-12 22:15:10 -0500

    debugging

--- LargestRectangleInHistogram.py
+++ LargestRectangleInHistogram.py
@@ -15,11 +15,31 @@ class Solution:
         print(tempStack)
 
         #left with monotonically increasing tempStack - record their areas
-        for i in range(0, len(tempStack)):
-            ind = tempStack[i]
-            if i == 0:
-                areas[ind[0]] = n * heights[ind[0]] #smallest overall
-            else:
-                areas[ind[0]] = (n - ind[0]) * heights[ind[0]]
-        print(areas)
+        bound = self.findZeros(heights)
+
+        left = 0
+        while left != len(bound) - 1
+            for i in range(bound[left], bound[left+1]):
+                ind = tempStack[i]
+                if i == 0:
+                    areas[ind[0]] = (bound[j+1]-bound[j]) * heights[ind[0]] # smallest overall in component
+                else:
+                    areas[ind[0]] = (n - ind[0]) * heights[ind[0]]
+            left += 1
+
         return max(areas)
+
+    def findBoundaries(self, nums) -> List[int]
+        res = []
+        n = len(nums)
+        if nums[0]!= 0:
+            res.append(0)
+
+        for i in range(1, n-1):
+            if nums[i] != 0 and (nums[i-1]==0 or nums[i+1]==0):
+                res.append(i)
+
+        if nums[n-1]!=0:
+            res.append(n-1)
+
+        return res

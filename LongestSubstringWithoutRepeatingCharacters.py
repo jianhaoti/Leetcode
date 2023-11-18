b8449c6eb69b761093e934846196091a068a12ef
Paul Tee <jianhaoti@gmail.com>
2023-11-17 23:17:19 -0500

    fast now. only one dict

--- LongestSubstringWithoutRepeatingCharacters.py
+++ LongestSubstringWithoutRepeatingCharacters.py
@@ -3,14 +3,15 @@ class Solution:
         myDict = {} #flip s
         n = len(s)
         res = 0
+        start = 0
 
         for i in range(0, n):
-            if s[i] in myDict:
-                res = max(res, len(myDict))
-                myDict = {key:value for key,value in myDict.items() if value > myDict[s[i]]}
+            if s[i] in myDict and myDict[s[i]] >= start:
+                res = max(res, i - start)
+                start = myDict[s[i]]+1
             myDict[s[i]] = i
 
-        res = max(res,len(myDict))
+        res = max(res, n - start)
 
         return res
 

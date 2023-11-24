bb9ffdfd3ade54b648a3048f965d22145f0ab6d4
Paul Tee <jianhaoti@gmail.com>
2023-11-24 12:55:52 -0500

    WIP

--- LongestRepeatingCharacterReplacement.py
+++ LongestRepeatingCharacterReplacement.py
@@ -6,9 +6,20 @@ class Solution:
             if not starts or s[i] != starts[-1]:
                 starts.append(i)
 
-        for i in range(n):
+        maxWindow = 1
+        same = 0
+        for i in range(n): # just move the window, don't destroy s. destroying is incompatible with starts
             if s[i] == starts[0]:
                 letter = starts.pop(0)
 
+                if n - i >= maxWindow: # if i can fit a window
+                    change = s[0,maxWindow].count(letter)
+                    nonOccurences = maxWindow - occurences
+                    change = k -  nonOccurences
 
-        return res
+                    while change >= 0:
+                        if
+
+
+
+        return maxWindow
